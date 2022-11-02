00122b63a77fa59bf0ee20ab90bdf25ba759112f
Navneet Bende <43838412+NavneetBende@users.noreply.github.com>
2022-11-03 01:17:20 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,14 @@
+class Division
+{
+	public static void main(String[] args) 
+	{
+		float a = 9.5f;
+		float b = 4.5f;
+		float c = 2.5f;
+		float d = 3;
+		float e = 45.5f;
+		float f = 3.5f;
+		double result = (a*b*c*d)/(e*f);
+		System.out.println("Final Result:" +result);
+	}
+}
